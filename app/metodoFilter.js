747ddd20de6d68beeb57834b0f3f8f4110a77065
Caio Cezar <caio.rcezar@gmail.com>
2023-03-23 10:05:46 -0300

    metodos Reduce e Sort

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -7,6 +7,24 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros(){
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value 
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria);
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade(): filtrarPorCategoria(categoria);
     exibirLivrosNaTela(livrosFiltrados)
+    if(categoria == 'disponivel'){
+        const valorTotal = calcularValorTotalDisponiveis(livrosFiltrados)
+        exibirValorTotalDisponiveis(valorTotal)
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValorTotalDisponiveis(valorTotal){
+    elementoComValorTotalDisponiveis.innerHTML = ` <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>`
 }
--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,10 +1,13 @@
 const elementoParaInserirLivros = document.getElementById('livros');
+const elementoComValorTotalDisponiveis = document.getElementById('valor_total_livros_disponiveis')
 
 function exibirLivrosNaTela(listaDeLivros){
+    elementoComValorTotalDisponiveis.innerHTML = '';
     elementoParaInserirLivros.innerHTML = '';
     listaDeLivros.forEach(livro => {
+        let disponibilidade = livro.quantidade > 0 ? "livro_imagens" : "livro_imagens indisponivel"
         elementoParaInserirLivros.innerHTML += `<div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
         <h2 class="livro__titulo">
          ${livro.titulo}
         </h2>
